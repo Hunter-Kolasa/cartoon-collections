4f9ca60449448c325f2e3ada7b6941abc2eb48f0
Hunter <kolasa.hunter@aol.com>
2020-11-12 20:04:17 -0500

    commit tree aa09ca84be221943ee95baf4fa39e8f494d087bf

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,28 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+
+def roll_call_dwarves(names)
+   i = 0
+   while i < names.length
+    puts "#{i + 1}. #{names}"
+    i += 1
+   end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(calls)
+  calls.map {|call| "#{call.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  if calls.any? {|call| call.length > 4}
+    true
+  else
+    false
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(stuff)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  stuff.find do |things|
+    cheese_types.include?(things)
+  end
 end
